41df50da866fff17ca4e68921524d7fa9976005e
sanket-s-thorat <devilmind201603116@gmail.com>
2023-09-03 22:54:28 +0530

    Added Star Canvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,16 +1,13 @@
-import { React, useState, useRef, Suspense } from "react";
+import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = ({ props }) => {
+const Stars = (props) => {
   const ref = useRef();
-
-  const sphere = (props) => {
-    const ref = useRef();
-
-    const sphere = random.inSphere(new Float32Array(5000), { rdius: 1.2 });
-  };
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
 
   useFrame((state, delta) => {
     ref.current.rotation.x -= delta / 10;
@@ -19,14 +16,14 @@ const Stars = ({ props }) => {
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} position={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
           color="#f272c8"
-          size={0.02}
+          size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
-        ></PointMaterial>
+        />
       </Points>
     </group>
   );
