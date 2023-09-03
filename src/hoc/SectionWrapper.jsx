f16763a0e644552e5ad789e910040fc0a1342aa1
sanket-s-thorat <devilmind201603116@gmail.com>
2023-09-03 22:47:01 +0530

    Complete

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import { motion } from "framer-motion";
-import { staggerContainer } from "../utils/motion";
-import { styles } from "../styles";
 
-const StarWrapper = (Component, idName) => {
+import { styles } from "../styles";
+import { staggerContainer } from "../utils/motion";
+
+const StarWrapper = (Component, idName) =>
   function HOC() {
     return (
       <motion.section
@@ -14,13 +14,13 @@ const StarWrapper = (Component, idName) => {
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         <span className="hash-span" id={idName}>
-          {" "}
           &nbsp;
         </span>
+
         <Component />
       </motion.section>
     );
-  }
-};
+  };
 
 export default StarWrapper;
+    
