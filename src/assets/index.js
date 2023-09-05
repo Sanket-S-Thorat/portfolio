79231cfd1e5932be005fb89b2c5885e9b3e6c06b
sanket-s-thorat <devilmind201603116@gmail.com>
2023-09-05 14:49:46 +0530

    edited exports

--- src/assets/index.js
+++ src/assets/index.js
@@ -9,17 +9,10 @@ import close from "./close.svg";
 
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
 import git from "./tech/git.png";
-import html from "./tech/html.png";
 import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
 import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 
 import ansible from "./tech/ansible.png";
 import aws from "./tech/aws.svg";
@@ -30,11 +23,6 @@ import linux from "./tech/linux.svg";
 import devops from "./tech/devops.png";
 import python from "./tech/python.png";
 import kubernetes from "./tech/kubernetes.svg";
-
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 
 import infosys from "./company/infosys.png";
 import tcs from "./company/tcs.png";
@@ -54,17 +42,10 @@ export {
   close,
   css,
   docker,
-  figma,
   git,
-  html,
   javascript,
-  mongodb,
-  nodejs,
   reactjs,
-  redux,
-  tailwind,
   typescript,
-  threejs,
   aws,
   azure,
   terraform,
@@ -74,10 +55,6 @@ export {
   ansible,
   devops,
   kubernetes,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
   infosys,
   tcs,
   carrent,
