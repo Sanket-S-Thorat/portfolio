b2dcd59f08083beaf5dbba208c9dd6612969d5ef
sanket-s-thorat <devilmind201603116@gmail.com>
2023-09-05 14:11:07 +0530

    added img

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,6 +21,8 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
+impoer
+
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
